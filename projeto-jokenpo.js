3a3563b536f25a24130aa4176e484c00e4926334
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-06-01 21:08:42 -0300

    adiciona a funcionalidade de rodadas desejadas e quem obteve mais vitorias em todas essas rodadas

--- projeto-jokenpo.js
+++ projeto-jokenpo.js
@@ -7,54 +7,80 @@ console.log(`ESCOLHA SUA JOGADA E O COMPUTADOR IRÁ ESCOLHER ALEATORIAMENTE!
 [1]- PAPEL
 [2]- TESOURA`);
 
-let escolhaUser = +prompt("Digite sua escolha: ");
+let qtdRodadas = +prompt("Digite quantas rodadas você deseja jogar: ");
 
-let escolhaComputador = Math.round(Math.random() * 2);
+let winsUser = 0;
 
-if (escolhaUser == 0) {
-  if (escolhaComputador == 0) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, temos empate!`
-    );
-  } else if (escolhaComputador == 1) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto,  computador venceu!`
-    );
-  } else if (escolhaComputador == 2) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, você venceu!`
-    );
+let winsComputer = 0;
+
+let empates = 0;
+
+for (let i = 0; i < qtdRodadas; i++) {
+  let escolhaUser = +prompt("Digite sua escolha: ");
+  let escolhaComputador = Math.round(Math.random() * 2);
+
+  if (escolhaUser == 0) {
+    if (escolhaComputador == 0) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, temos empate!`
+      );
+      empates++;
+    } else if (escolhaComputador == 1) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto,  computador venceu!`
+      );
+      winsComputer++;
+    } else if (escolhaComputador == 2) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, você venceu!`
+      );
+      winsUser++;
+    }
+  }
+
+  if (escolhaUser == 1) {
+    if (escolhaComputador == 0) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, você venceu!`
+      );
+      winsUser++;
+    } else if (escolhaComputador == 1) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, temos empate!`
+      );
+      empates++;
+    } else if (escolhaComputador == 2) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, computador venceu!`
+      );
+      winsComputer++;
+    }
+  }
+
+  if (escolhaUser == 2) {
+    if (escolhaComputador == 0) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, computador venceu!`
+      );
+      winsComputer++;
+    } else if (escolhaComputador == 1) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, você venceu!`
+      );
+      winsUser++;
+    } else if (escolhaComputador == 2) {
+      console.log(
+        `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, temos empate!`
+      );
+      empates++;
+    }
   }
 }
 
-if (escolhaUser == 1) {
-  if (escolhaComputador == 0) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, você venceu!`
-    );
-  } else if (escolhaComputador == 1) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, temos empate!`
-    );
-  } else if (escolhaComputador == 2) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, computador venceu!`
-    );
-  }
-}
-
-if (escolhaUser == 2) {
-  if (escolhaComputador == 0) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, computador venceu!`
-    );
-  } else if (escolhaComputador == 1) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, você venceu!`
-    );
-  } else if (escolhaComputador == 2) {
-    console.log(
-      `Você escolheu ${jogadas[escolhaUser]} e o computador escolheu ${jogadas[escolhaComputador]}, portanto, temos empate!`
-    );
-  }
+if(winsComputer > winsUser){
+  console.log(`O computador teve ${winsComputer} vitórias e você teve ${winsUser} vitórias, portanto o computador foi campeão!`);
+}else if(winsComputer < winsUser){
+  console.log(`Você teve ${winsUser} vitórias e o computador teve ${winsComputer} vitórias, portanto você foi o campeão!`);
+}else if(winsComputer==winsUser){
+  console.log(`Você teve ${winsUser} vitórias e o computador teve ${winsComputer} vitórias, portanto, temos um empate!`);
 }
